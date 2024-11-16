52c19d0c9936f159f89a4ddaab39ae2332f61c78
Syed Farith <127757606+syedfarith@users.noreply.github.com>
2024-11-16 12:10:44 +0530

    port added

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def get_recommendations():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
